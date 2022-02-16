c3a8802ec67fe1dcf5af0f47a414304a4938263f
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-02-16 20:31:43 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+//          psvm
+    public static void main(String[] args){
+//          sout
+//        System.out.println("Rossita");
+
+        int newInt1;
+         newInt1 = 10;
+
+//  datatype address  value
+        int newInt2 = 15;
+
+        double newDouble1 = 2.3;
+        double newDouble2 = 3; //3.0
+
+        boolean newBoolean1 = true;
+        boolean newBoolean2 = false;
+
+        char newChar1 = 'a';
+        char newChar2 = '@';
+
+        String newString1 = "This is my string";
+        String newString2 = "^&**&*56757";
+        String newString3 = "Word";
+        String newString4 = "#";
+
+        System.out.println(newString1);
+
+    }
+}
+
