d2e293c8d83b20f59cd293f146259561e5481936
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-02-20 19:48:50 +0200

    1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,20 @@ package com.company;
 
 public class Main {
 //          psvm
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //          sout
 //        System.out.println("Rossita");
 
         int newInt1;
-         newInt1 = 10;
+        newInt1 = 10;
 
 //  datatype address  value
         int newInt2 = 15;
+        int newInt3 = 8, newInt4 = 7, newInt5 = 9;
 
         double newDouble1 = 2.3;
-        double newDouble2 = 3; //3.0
+        double newDouble2 = 3; // 3.0
+        double newDouble3 = 6.7, newDouble4 = 7.9;
 
         boolean newBoolean1 = true;
         boolean newBoolean2 = false;
@@ -21,13 +23,38 @@ public class Main {
         char newChar1 = 'a';
         char newChar2 = '@';
 
+
         String newString1 = "This is my string";
         String newString2 = "^&**&*56757";
-        String newString3 = "Word";
-        String newString4 = "#";
+        String newString3= "Word";
+        String newString4 = "#", newString5 = "1 + 2";
 
-        System.out.println(newString1);
+        newString3.length(); // = 4
+
+        int result1 = 1 + 2, result2 = 1 - 2, result3 = 1 * 2, result4 = 1 / 2;
+        int result5 = newInt1 + 2, result6 = newInt2 + newInt3;
+
+        String newString6 = "Hi " + "How " + "Are " + "You ";
+
+//        System.out.println(newString6);
+//        System.out.println("Hi " + "How " + "Are " + "You");
+//        System.out.println(newString2 + " " + newString3);
+//
+//
+//        String name1 = "Rossita";
+//        String name2 = "Suzan";
+//        System.out.println("Hello dear " + name1 +  ". We are happy that you are here");
+//        System.out.println("Hello dear " + name2 +  ". We are happy that you are here");
+        System.out.println(newInt1 + 2); // 10 + 2 = 12
+        System.out.println("" + newInt1 + 2); // "10" + "2"
+        System.out.println(newInt1 + " + " + 2); // "10" + "! " + "2"
+
+
+
 
     }
+
 }
+
+
 
